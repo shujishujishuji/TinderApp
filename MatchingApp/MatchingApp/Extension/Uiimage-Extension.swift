f28d2ec0460062747a9ff087086b320fc298458c
shujishujishuji <shuji00881199@gmail.com>
2021-09-15 07:19:49 +0900

    bottomButton追加

---
+++ MatchingApp/MatchingApp/Extension/Uiimage-Extension.swift
@@ -0,0 +1,27 @@
+//
+//  Uiimage-Extension.swift
+//  MatchingApp
+//
+//  Created by taniguchi shuji on 2021/09/15.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func resize(size _size: CGSize) -> UIImage? {
+        let widthRatio = _size.width / size.width
+        let heightRatio = _size.height / size.height
+        let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+        
+        let resizeSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(resizeSize, false, 0.0)
+        draw(in: CGRect(origin: .zero, size: resizeSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedImage
+    }
+    
+}
