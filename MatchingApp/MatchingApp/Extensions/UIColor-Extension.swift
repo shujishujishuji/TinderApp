9b75f848eec9d4c2b3e687d071b65459ec3450ab
shujishujishuji <shuji00881199@gmail.com>
2021-09-16 02:52:31 +0900

    cardviewのgestureの追加

---
+++ MatchingApp/MatchingApp/Extensions/UIColor-Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor-Extension.swift
+//  MatchingApp
+//
+//  Created by taniguchi shuji on 2021/09/16.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
+        return .init(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1)
+    }
+}
--- MatchingApp/MatchingApp/Views/CardView.swift
+++ MatchingApp/MatchingApp/Views/CardView.swift
@@ -13,7 +13,9 @@ class CardView: UIView {
         let iv = UIImageView()
         iv.backgroundColor = .red
         iv.layer.cornerRadius = 10
-        iv.contentMode = .scaleToFill
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(named: "saya")
+        iv.clipsToBounds = true
         return iv
     }()
     
@@ -57,6 +59,36 @@ class CardView: UIView {
         return label
     }()
     
+    let goodLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 45)
+        label.text = "good"
+        label.textColor = .rgb(red: 137, green: 223, blue: 86)
+        
+        label.layer.borderWidth = 3
+        label.layer.borderColor = UIColor.rgb(red: 137, green: 223, blue: 86).cgColor
+        label.layer.cornerRadius = 10
+        
+        label.textAlignment = .center
+        label.alpha = 0
+        return label
+    }()
+    
+    let nopeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 45)
+        label.text = "nope"
+        label.textColor = .rgb(red: 222, green: 110, blue: 110)
+        
+        label.layer.borderWidth = 3
+        label.layer.borderColor = UIColor.rgb(red: 222, green: 110, blue: 110).cgColor
+        label.layer.cornerRadius = 10
+        
+        label.textAlignment = .center
+        label.alpha = 0
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -70,14 +102,42 @@ class CardView: UIView {
         let translation = gesture.translation(in: self)
         
         if gesture.state == .changed {
-            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+            
+            self.handlePanChange(translation: translation)
+        
         } else if gesture.state == .ended {
-            UIView.animate(withDuration: 0.3) {
-                self.transform = .identity
-                self.layoutIfNeeded()
+            self.handlePanEnded()
             }
         }
+    
+    private func handlePanChange(translation: CGPoint) {
+        let dgree: CGFloat = translation.x / 20
+        let angle = dgree * .pi / 100
+        
+        let rotateTranslation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotateTranslation.translatedBy(x: translation.x, y: translation.y)
+        
+        let ratio: CGFloat = 1 / 100
+        let ratioValue = ratio * translation.x
+        
+        if translation.x > 0 {
+            self.goodLabel.alpha = ratioValue
+        } else if translation.x < 0 {
+            self.nopeLabel.alpha = -ratioValue
+        }
     }
+    
+    private func handlePanEnded() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.7, options: []) {
+            self.transform = .identity
+            self.layoutIfNeeded()
+            self.goodLabel.alpha = 0
+            self.nopeLabel.alpha = 0
+            
+        }
+    }
+    
+
     
     private func setupLayout() {
         let infoVerticalSatckView = UIStackView(arrangedSubviews: [resindenceLabel, hobbyLabel, introductionLabel])
@@ -89,11 +149,15 @@ class CardView: UIView {
         addSubview(cardImageView)
         addSubview(nameLabel)
         addSubview(baseStackView)
+        addSubview(goodLabel)
+        addSubview(nopeLabel)
         
         cardImageView.anchor(top: topAnchor, bottom: bottomAnchor, left: leftAnchor, right: rightAnchor, leftPadding: 10, rightPadding: 10)
         infoButton.anchor(width: 40)
         baseStackView.anchor(bottom: cardImageView.bottomAnchor, left: cardImageView.leftAnchor, right: cardImageView.rightAnchor, bottomPadding: 40, leftPadding: 20, rightPadding: 20)
         nameLabel.anchor(bottom: baseStackView.topAnchor, left: cardImageView.leftAnchor, bottomPadding: 10, leftPadding: 20)
+        goodLabel.anchor(top: cardImageView.topAnchor, left: cardImageView.leftAnchor, width: 140, height: 55, topPadding: 25, leftPadding: 20)
+        nopeLabel.anchor(top: cardImageView.topAnchor, right: cardImageView.rightAnchor, width: 140, height: 55, topPadding: 25, leftPadding: 20)
     }
     
     required init?(coder: NSCoder) {
